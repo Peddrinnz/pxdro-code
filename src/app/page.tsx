ac404c433d3afebe74bc89900f617c9a7b222d65
Peddrinnz <pedroesnarriaga@gmail.com>
2025-11-12 22:52:16 -0400

    feat: create components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/custom/header";
+
 export default function Home() {
   return (
     <div>
       <main>
+        <Header />
         <div>
           <h1 className="text-3xl font-bold underline">
             Hello world!
